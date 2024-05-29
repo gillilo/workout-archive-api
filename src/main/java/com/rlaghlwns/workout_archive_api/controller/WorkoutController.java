140c11a325daadab9b0f4e2965c47cd81d1bf2ff
gillilo <ghlwns1535@gmail.com>
2024-05-29 15:52:59 +0900

    1

--- src/main/java/com/rlaghlwns/workout_archive_api/controller/WorkoutController.java
+++ src/main/java/com/rlaghlwns/workout_archive_api/controller/WorkoutController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/workout")
+@RequestMapping("/workout")
+@RestController
 @RequiredArgsConstructor
 public class WorkoutController {
 
